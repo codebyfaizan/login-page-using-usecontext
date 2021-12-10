b5dd2536db689f944b6525d58962da98c819d1d0
Faizan <faizan.lko888@gmail.com>
2021-12-10 21:48:06 +0530

    post deployment changes

---
+++ src/store/auth-context.js
@@ -0,0 +1,43 @@
+import React from "react";
+
+const AuthContext = React.createContext({
+  isLoggedIn: false,
+  onLogout: () => {},
+  onLogin: (email, password) => {},
+});
+
+export const AuthContextProvider = (props) => {
+  const [isLoggedIn, setIsLoggedIn] = React.useState(false);
+
+  React.useEffect(() => {
+    const storedUserLoggedInInformation = localStorage.getItem("isLoggedIn");
+
+    if (storedUserLoggedInInformation === "1") {
+      setIsLoggedIn(true);
+    }
+  }, []);
+
+  function loginHandler() {
+    localStorage.setItem("isLoggedIn", "1");
+    setIsLoggedIn(true);
+  }
+
+  function logoutHandler() {
+    localStorage.removeItem("isLoggedIn");
+    setIsLoggedIn(false);
+  }
+
+  return (
+    <AuthContext.Provider
+      value={{
+        isLoggedIn: isLoggedIn,
+        onLogout: logoutHandler,
+        onLogin: loginHandler,
+      }}
+    >
+      {props.children}
+    </AuthContext.Provider>
+  );
+};
+
+export default AuthContext;
